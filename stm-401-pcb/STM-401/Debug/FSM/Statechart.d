2d4594c9eddbb93047192be993d33edaa0aca44d
Stacionaras <85024601+Karosas-karpis@users.noreply.github.com>
2026-05-20 17:20:36 +0300

    Revert "Folder structure"
    This reverts commit d62671799b87b2967d87677cb9eb45596db2a7da.

---
+++ stm-401-pcb/STM-401/Debug/FSM/Statechart.d
@@ -0,0 +1,5 @@
+FSM/Statechart.o: ../FSM/Statechart.c ../FSM/../src/sc_types.h \
+ ../FSM/Statechart.h ../FSM/Statechart_required.h
+../FSM/../src/sc_types.h:
+../FSM/Statechart.h:
+../FSM/Statechart_required.h:
